dbefc6754390587ff9a7e16dbb1906dd5ae55824
maxirosanda <maxi_rosanda@hotmail.com>
2021-08-18 20:27:24 -0300

    corregir error borrar ( borraba todo)

--- App.js
+++ App.js
@@ -1,35 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View,TextInput,Button, FlatList } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button, FlatList ,Modal} from 'react-native';
 
 export default function App() {
   const [producto, setProducto] = useState("");
   const [productos, setProductos] = useState([]);
-
+  const [visible, setVisible] = useState(false);
+  const [capturarId, setCapturarId] = useState("");
   const capturar = (t) =>{
     setProducto(t)
   }
   const agregar = () =>{
     setProductos([...productos,{_id:Math.random().toString(),producto:producto}])
-    console.log(productos)
+    setProducto("")
+  }
+
+  const abrirModal = (id) =>{
+    setCapturarId(id)
+    setVisible(true)
+  }
+  const borrar = () =>{
+
+  setProductos(productos.filter(item =>item._id !== capturarId))
+  setVisible(false)
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.containerimput}> 
-        <TextInput style={styles.imput} onChangeText={capturar} placeholder="ingrese Producto" />
-        <Button title="Enviar" color="#000" onPress={agregar} />
+        <TextInput style={styles.imput} onChangeText={capturar} value={producto} placeholder="ingrese Producto" />
+        <Button title="Enviar"  color="#000" onPress={agregar} />
         </View>
     <FlatList style={styles.flat} data={productos} keyExtractor={item => item._id} renderItem={(data) => (
         
         <View style={styles.containerimput}>
         <Text style={styles.text} >{data.item.producto}</Text>
-        <View style={styles.contenedorbutton}><Button title="x"></Button></View>
+        <View style={styles.contenedorbutton}><Button onPress={()=> abrirModal(data.item._id)} title="x"></Button></View>
         </View>
       )
     }/>
-
+     <Modal animationType="slide" visible={visible}>
+      <Text>Esta seguro que desea borrar el item</Text>
+      <Button title="Borrar" onPress={borrar}></Button>
+     </Modal>
     </View>
+   
     
   );
 }
@@ -55,7 +70,7 @@ const styles = StyleSheet.create({
   text:{
     margin:20,
     padding:5,
-    width:"75%",
+    width:"35%",
     textAlign:"center",
     borderWidth:2
   },
